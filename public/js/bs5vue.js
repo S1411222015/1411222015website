3bb70ebb1b6e66a4104878e250e495d03fe6ce80
tingggg0701 <s1411222015@nutc.edu.tw>
2026-01-13 21:56:26 +0800

    0113

--- public/js/bs5vue.js
+++ public/js/bs5vue.js
@@ -1,27 +1,78 @@
-const { createApp, ref, onMounted } = Vue;
+const { createApp, ref } = Vue;
 
-createApp({
-  setup() {
-    const Portfolio = ref([]);
-    const newPortfolio = ref({ title:'', description:'', image:'', href:'#' });
+var portfolioApp = Vue.createApp({
+    data() {
+        return {
+            portfolio: [],
 
-    const fetchPortfolio = async () => {
-      try {
-        const res = await axios.get("/portfolio"); // 相對路徑
-        Portfolio.value = res.data;
-      } catch(err) { console.error(err); }
-    };
+            // 對應表單 v-model
+            newPortfolio: {
+                title: "",
+                description: ""
+            }
+        }
+    },
 
-    const addPortfolio = async () => {
-      try {
-        const res = await axios.post("/portfolio", newPortfolio.value);
-        Portfolio.value.push(res.data);
-        newPortfolio.value = { title:'', description:'', image:'', href:'#' };
-      } catch(err) { console.error(err); }
-    };
+    methods: {
+        // 抓取作品列表
+        fetchPortfolio() {
+            $.ajax({
+                url: "/portfolio",
+                method: "get",
+                dataType: "json",
+                success: (result) => {
+                    this.portfolio = result; // 直接使用後端資料
+                },
+                error: (err) => console.error("抓作品列表失敗", err)
+            });
+        },
 
-    onMounted(fetchPortfolio);
+        // 新增作品
+        async addPortfolio() {
+            try {
+                const form = document.querySelector("#addWorkForm form");
+                const formData = new FormData(form);
 
-    return { Portfolio, newPortfolio, addPortfolio };
-  }
-}).mount("#app");
+                const response = await fetch("/portfolio_add", {
+                    method: "POST",
+                    body: formData
+                });
+
+                const result = await response.json();
+
+                if (result.success) {
+                    alert("作品新增成功");
+
+                    // 清空表單
+                    this.newPortfolio.title = "";
+                    this.newPortfolio.description = "";
+                    
+
+                    // 將新增作品加入 portfolio（後端已回傳完整資料）
+                    const newItem = result.newItem;
+                    if (newItem) {
+                        this.portfolio.push({
+                            _id: newItem._id,
+                            title: newItem.title,
+                            text: newItem.text,
+                            imgSrc: newItem.imgSrc
+                        });
+                    }
+
+                } else {
+                    alert("新增失敗");
+                }
+
+            } catch (err) {
+                console.error(err);
+                alert("伺服器錯誤");
+            }
+        }
+    },
+
+    mounted() {
+        // 初始化抓作品
+        this.fetchPortfolio();
+    }
+    
+}).mount("#portfolio");
--- server.js
+++ server.js
@@ -1,54 +1,145 @@
-const express = require("express");
-const server = express();
-const bodyParser = require("body-parser");
-const path = require("path");
-const DB = require("nedb-promises");
+var express = require("express");
+var server = express();
+var bodyParser = require("body-parser");
 
-server.use(bodyParser.urlencoded({ extended: true }));
+
+server.set("view engine", 'ejs');
+server.set("views", __dirname+"/view")
+
+var fileUpload = require("express-fileupload");
+
+server.use(express.static(__dirname + "/Public"));
+server.use(bodyParser.urlencoded());
 server.use(bodyParser.json());
+server.use(fileUpload({limits:{fileSize:2*1024*1024}}))
 
-// 靜態檔案
-server.use(express.static(path.join(__dirname, "public")));
+var DB=require("nedb-promises");
+var ServiceDB = DB.create(__dirname+"/Service.db");
+var PortfolioDB = DB.create(__dirname+"/Portfolio.db");
+/*PortfolioDB.insert([
+    {
+        href: "#portfolio",
+        imgSrc: "/img/fu.jpg",
+        title: "葬送的芙莉蓮 (同人)",
+        text: "以《葬送のフリーレン》為題材的同人作品，同樣是因興趣所畫。",
+        order: 1
+      },
+      {
+        href: "#portfolio",
+        imgSrc: "/img/lucy.jpg",
+        title: "電馭叛客：邊緣行者",
+        text: "描繪《サイバーパンク エッジランナーズ》中的角色露西，作品出於個人興趣。",
+        order: 2
+      },
+      {
+        href: "#portfolio",
+        imgSrc: "/img/Idol.jpg",
+        title: "我推的孩子 (同人)",
+        text: "這是我創作的 2D 插畫作品《推しの子》，在自己的閒暇時間完成。",
+        order: 3
+      },
+      {
+        href: "#portfolio",
+        imgSrc: "/img/PAWA.jpg",
+        title: "鏈鋸人.帕瓦 (同人)",
+        text: "《チェンソーマン》的角色血之惡魔帕瓦，創作源於個人興趣。",
+        order: 4
+      },
+      {
+        href: "#portfolio",
+        imgSrc: "/img/BOL.png",
+        title: "BOL繪畫小專題",
+        text: "大一繪畫課和組員們一起創作的遊戲角色設計海報，我設計的角色是[極]",
+        order: 5
+      },
+      {
+        href: "#portfolio",
+        imgSrc: "/img/makima.jpg",
+        title: "鏈鋸人",
+        text: "鏈鋸人插畫，可作為書籤樣式，也屬於興趣創作。",
+        order: 6
+      },
+      {
+        href: "#portfolio",
+        imgSrc: "/img/clove_pant.jpg",
+        title: "clove珂樂芙",
+        text: "以《Valorant》特務clove為題，採用厚塗手法，屬較早期作品，當時厚塗技巧尚在磨練中。",
+        order: 7
+      },
+      {
+        href: "#portfolio",
+        imgSrc: "/img/see_that.jpg",
+        title: "人物創圖 (系列圖)",
+        text: "自己創作的角色宣傳圖，是一系列作品的一部分，其餘部分尚未完成。",
+        order: 8
+      },
+      {
+        href: "#portfolio",
+        imgSrc: "/img/jett.jpg",
+        title: "JETT婕提",
+        text: "以《Valorant》特務JETT為題，使用厚塗手法創作，源自對角色的喜愛。",
+        order: 9
+      }
 
-const ServiceDB = DB.create(__dirname + "/Service.db");
+    
+]);
+*/
 
-// ===== 初始化 Portfolio 資料 =====
-async function initPortfolio() {
-  const exists = await ServiceDB.find({ type: "portfolio" });
-  if (exists.length === 0) {
-    await ServiceDB.insert([
-      { type:"portfolio", title:"我推的孩子(同人)", description:"這是我創作的 2D 插畫作品【推しの子】自己閒餘時間所創作。", image:"img/Idol.jpg", href:"#portfolioModal1" },
-      { type:"portfolio", title:"葬送的芙莉蓮(同人)", description:"葬送のフリーレン，同為自己興趣所畫。", image:"img/fu.jpg", href:"#portfolioModal2" },
-      { type:"portfolio", title:"鏈鋸人.帕瓦(同人)", description:"チェンソーマン的角色血之惡魔，因自己興趣所畫。", image:"img/PAWA.jpg", href:"#portfolioModal3" },
-      { type:"portfolio", title:"電馭叛客：邊緣行者", description:"サイバーパンク エッジランナーズ裡的角色露西，興趣所畫。", image:"img/lucy.jpg", href:"#portfolioModal4" },
-      { type:"portfolio", title:"鏈鋸人", description:"鏈鋸人插畫是可以做成書籤的樣式，也是興趣圖。", image:"img/makima.jpg", href:"#portfolioModal5" },
-      { type:"portfolio", title:"JETT婕提", description:"valorant裡的特務之一，也是因為很喜歡所以才畫，是採用厚塗手法創作。", image:"img/jett.jpg", href:"#portfolioModal6" },
-      { type:"portfolio", title:"人物創圖(系列圖)", description:"自己所創作的角色，宣傳圖，他是一系列的不過其他的還沒完成", image:"img/see_that.jpg", href:"#portfolioModal7" },
-      { type:"portfolio", title:"clove珂樂芙", description:"valorant裡的特務之一，採用厚塗手法創作，屬於比較前期的作品，當時厚塗手法不是很熟練", image:"img/clove_pant.jpg", href:"#portfolioModal8" },
-      { type:"portfolio", title:"BOL繪畫小專題", description:"大一繪畫課和組員們一起創作的遊戲角色設計海報，我設計的角色是[極]", image:"img/BOL.png", href:"#portfolioModal9" }
-    ]);
-  }
-}
+server.get("/services", (req, res) => {
+    
+    ServiceDB.find({},{_id:0}).then(results=>{
+       
+        res.send(results);
+    })
+    
+})
 
-initPortfolio();
-
-// 根目錄
-server.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+server.get("/portfolio", (req, res) => {
+    PortfolioDB.find({}).then(results => {
+        res.send(results);
+    });
 });
 
-// portfolio API
-server.get("/portfolio", async (req, res) => {
-  const Portfolio = await ServiceDB.find({ type: "portfolio" });
-  res.json(Portfolio);
+server.get("/showServices",(req,res)=>{
+    ServiceDB.find({},{_id:0}).then(results=>{
+       
+        res.render("service",{Services:results});
+    }).catch(error=>{
+
+    })
+
+})
+
+
+
+
+server.post("/portfolio_add", async (req, res) => {
+    try {
+        let imgSrc = "";
+
+        if (req.files && req.files.imageFile) {
+            const file = req.files.imageFile;
+            await file.mv(__dirname + "/Public/upload/" + file.name);
+            imgSrc = "/upload/" + file.name;
+        }
+
+        // 非同步等待 DB 插入完成，並取得 _id
+        const newItem = await PortfolioDB.insert({
+            title: req.body.title,
+            text: req.body.description,
+            imgSrc: imgSrc
+        });
+
+        // 回傳完整資料給前端
+        res.send({ success: true, newItem });
+
+    } catch (err) {
+        console.error(err);
+        res.status(500).send({ success: false, message: "伺服器錯誤" });
+    }
 });
 
-// 新增作品 API
-server.post("/portfolio", async (req, res) => {
-  const newPortfolio = { type: "portfolio", ...req.body };
-  const inserted = await ServiceDB.insert(newPortfolio);
-  res.json(inserted);
-});
+
 
 server.listen(8090, () => {
   console.log("Server running on http://localhost:8090");
